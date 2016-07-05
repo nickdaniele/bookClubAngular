3db0ff9f156118a62b23a8009f69240f18f44657
nick_d <ngdaniele@gmail.com>
2016-07-05 15:43:53 -0700

    Got data from api filtered

--- public/main.js
+++ public/main.js
@@ -1,11 +1,47 @@
 angular.module('searchModule', [])
-  .controller('searchController', function($scope) {
-    $scope.userReq = '';
-    $scope.submit = function() {
+  .factory('searchFactory', function($http) {
+    var makeUrl = function(controlScope) {
+      var userReq = controlScope.split(' ').join('+');
+      var userUrl = "https://www.googleapis.com/books/v1/volumes?q=" + userReq;
+      makeRequest(userUrl);
+    };
+    var makeRequest = function(userUrl) {
+      $http({
+        method: 'GET',
+        url: userUrl
+      }).then(function filterData(response) {
+          var response = response.data.items;
+          var filteredResults = [];
+          for (var i = 0; i < response.length; i++) {
+            var volInfo = response[i].volumeInfo;
+            if ("title" in volInfo && "authors" in volInfo && "imageLinks" in volInfo && "pageCount" in volInfo) {
+              filteredResults.push(volInfo);
+            }
+          }
+        }, function errorCallback(response) {
+          console.log('Error Getting Data');
+        });
+    };
+
+    return { makeUrl: makeUrl };
+  })
+
+  .controller('searchController', function($scope, searchFactory) {
+    $scope.reqString = '';
+    $scope.userReq = function() {
       if ($scope.text) {
-        $scope.userReq = this.text;
+        $scope.reqString = this.text;
         $scope.text = '';
       }
-    console.log($scope.userReq);
+      searchFactory.makeUrl($scope.reqString);
     };
   });
+
+
+// response.data.items
+// response.data.items[0]
+//   - volumeInfo
+//     - .title
+//     - .authors[0]
+//     - .imagelinks.thumbnail
+//     - .pagecount
