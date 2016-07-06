7dcb8b4294ab836c72fce47f2f604421d16c7fc1
nick_d <ngdaniele@gmail.com>
2016-07-05 19:22:38 -0700

    Got base app done, time for heroku

--- public/main.js
+++ public/main.js
@@ -13,15 +13,12 @@ angular.module('searchModule', [])
           var response = response.data.items;
           for (var i = 0; i < response.length; i++) {
             var volInfo = response[i].volumeInfo;
-            if ("title" in volInfo && "authors" in volInfo && "imageLinks" in volInfo && "pageCount" in volInfo) {
+            if ("title" in volInfo && "authors" in volInfo && "imageLinks" in volInfo && "pageCount" in volInfo && "description" in volInfo) {
               apiResponse.push(volInfo);
             }
           }
-          setTimeout(function() {
-            $scope.apiResponse = apiResponse;
-            $scope.$apply();
-            console.log($scope.apiResponse);
-          }, 50);
+          console.log(apiResponse);
+          $scope.apiResponse = apiResponse;
         }, function errorCallback(response) {
           console.log('Error Getting Data');
         });
@@ -41,48 +38,12 @@ angular.module('searchModule', [])
   });
 
 
-  // angular.module('searchModule', [])
-  //   .factory('searchFactory', function($http) {
-  //     var makeRequest = function(reqString, $scope) {
-  //       var userReq = reqString.split(' ').join('+');
-  //       var userUrl = "https://www.googleapis.com/books/v1/volumes?q=" + userReq;
-
-  //       $http({
-  //         method: 'GET',
-  //         url: userUrl
-  //       }).then(function filterData(response) {
-  //           var apiResponse = [];
-  //           var response = response.data.items;
-  //           for (var i = 0; i < response.length; i++) {
-  //             var volInfo = response[i].volumeInfo;
-  //             if ("title" in volInfo && "authors" in volInfo && "imageLinks" in volInfo && "pageCount" in volInfo) {
-  //               apiResponse.push(volInfo);
-  //             }
-  //           }
-  //           setTimeout(function() {
-  //             $scope.apiResponse = apiResponse;
-  //             $scope.$apply();
-  //             console.log($scope.apiResponse);
-  //           }, 1000);
-  //           // $scope.apiResponse = apiResponse;
-  //         }, function errorCallback(response) {
-  //           console.log('Error Getting Data');
-  //         });
-  //     };
-  //     return { makeRequest: makeRequest };
-  //   })
-
-  //   .controller('searchController', function($scope, searchFactory) {
-  //     $scope.userReq = function() {
-  //       var reqString = '';
-  //       if ($scope.text) {
-  //         reqString = this.text;
-  //         $scope.text = '';
-  //       }
-  //       searchFactory.makeRequest(reqString, $scope);
-  //       console.log($scope.apiResponse);
-  //     };
-  //   });
+  // Actually didn't need
+  // This is done to keep $scope correctly linked
+  // setTimeout(function() {
+  //   $scope.apiResponse = apiResponse;
+  //   $scope.$apply();
+  // }, 50);
 
 // response.data.items
 // response.data.items[0]
